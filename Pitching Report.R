96b4529330c98577d574d0d27af43b339a357c78
hankike <128631771+hankike@users.noreply.github.com>
2024-06-27 11:52:45 -0500

    Update Pitching Report.R

--- Pitching Report.R
+++ Pitching Report.R
@@ -4,6 +4,7 @@ library(ggridges)
 # Download data from baseball savant. Apply here.
 P <- read.csv("~/Downloads/665871_data.csv") #change player code
 
+# In order to condense our data, I created my own columns.
 columns = c("pitch_name", "pitch_type", "release_speed", "effective_speed", "release_spin_rate", "vx0", "vy0", "vz0",
             "ax", "ay", "az", "pfx_x", "pfx_z", "plate_x", "plate_z", "zone", "description")
 Pitches = P[, columns]
@@ -15,7 +16,7 @@ ggplot(Pitches, aes(release_speed, release_spin_rate)) +
              aes(fill = factor(pitch_type))) + 
   theme_classic() + 
   labs(x = "Pitch Speed (mph)", y = "Spin Rate (RPM)", title = "RPM vs Pitch Velocity", 
-       subtitle = "Javier Assad, 2024 season") + #change player name
+       subtitle = "*Pitcher Name*, 2024 season") + #change player name
   scale_fill_discrete(name = "pitch_type")
 
 # Second graph is pitch movement
@@ -26,7 +27,7 @@ ggplot(Pitches, aes(pfx_x_in, pfx_z_in)) +
   xlim(c(-25,25)) + #if pitcher a lefty use (25,-25). right use (-25,25)
   ylim(c(-25,25)) + 
   labs(x = "Horizontal Break (in)", y = "Vertical Break (in)", title = "Pitch Movement, Catcher Perspective", 
-       subtitle = "Javier Assad, 2024 Season") +
+       subtitle = "*Pitcher Name*, 2024 Season") +
   scale_fill_discrete(name = "pitch_type") +
   geom_hline(aes(yintercept = 0), lty = 2, col = "red", alpha = 0.5) + 
   geom_vline(aes(xintercept = 0), lty = 2, col = "red", alpha = 0.5) 
@@ -37,5 +38,5 @@ ggplot(Pitches, aes(x = release_speed, y = pitch_type, fill = pitch_type)) +
   theme_ridges(center_axis_labels = TRUE) + 
   scale_y_discrete(expand = expand_scale(add = c(0.2, 1))) +
   labs(x = "Velocity (mph)", y = NULL , title = "
-       Javier Assad Velocity Distribution") +
+       *Pitcher Name* Velocity Distribution") +
   theme(legend.position = "none")
